8ab41bfa1f95837c44409567abc0431142351425
fslprbw <fslprbw@gmail.com>
2016-10-05 23:59:39 +0700

    filter resample, build classifier

--- MyClassifiers/src/basic_function/filterResample.java
+++ MyClassifiers/src/basic_function/filterResample.java
@@ -5,10 +5,30 @@
  */
 package basic_function;
 
+import weka.core.Instances;
+import weka.filters.supervised.instance.Resample;
+import weka.filters.Filter;
+
 /**
  *
  * @author User
  */
 public class filterResample {
     
+    public static Instances filterResample(Instances ins) {
+        
+        final Resample filter = new Resample();
+        Instances filteredIns = null;
+        filter.setBiasToUniformClass(1.0);
+        try {
+            filter.setInputFormat(ins);
+            filter.setNoReplacement(false);
+            filter.setSampleSizePercent(100);
+            filteredIns = Filter.useFilter(ins, filter);
+	} catch (Exception e) {
+            System.out.println("Error when resampling input data!");
+            e.printStackTrace();
+	}
+	return filteredIns;
+    }
 }
