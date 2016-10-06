5d29a3f4d1dd3e4bdd3e907811b2eaef53f3f18e
aodyra <aodyra@gmail.com>
2016-10-06 18:11:03 +0700

    add MyID3

--- MyClassifiers/src/myclassifiers/MyClassifiers.java
+++ MyClassifiers/src/myclassifiers/MyClassifiers.java
@@ -5,17 +5,36 @@
  */
 package myclassifiers;
 
+import MyID3.MyID3;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+
 /**
  *
  * @author aodyra
  */
 public class MyClassifiers {
+    
+    public static MyID3 myID3 = new MyID3();
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, Exception {
         // TODO code application logic here
+        
+        BufferedReader reader = new BufferedReader(new FileReader("data/weather.nominal.arff"));
+        Instances data = new Instances(reader);
+        reader.close();
+        data.setClassIndex(4);
+        Evaluation eval = new Evaluation(data);
+        eval.crossValidateModel(myID3, data, 10, new Random(1));
+        System.out.println(eval.toSummaryString());
+        
     }
     
 }
